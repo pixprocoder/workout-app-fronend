0d0728fd72bb4e589ac26ad77d8ca8aa643af4dd
kabir <makkobir79@gmail.com>
2022-07-12 03:49:56 +0600

    firebase setup and social login completed

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../firebase.init";
 
 const Login = () => {
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  // const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
+
+  if (gUser) {
+    console.log(gUser);
+  }
+  if (gLoading) {
+    return <p>loading</p>;
+  }
+  if (gError) {
+    console.log(gError);
+  }
+
   return (
     <section>
       <div className="mt-4  w-50 mx-auto rounded bg-light">
@@ -8,7 +23,10 @@ const Login = () => {
           Login into workout-world
         </h1>
         <form class="p-4 ">
-          <button className="btn btn-outline-success d-block mx-auto w-50 my-2">
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline-success d-block mx-auto w-50 my-2"
+          >
             Continue with google
           </button>
           <button className="btn btn-outline-success d-block mx-auto w-50 my-2">
