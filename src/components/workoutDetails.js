a2a424c4905c044d7c4250525f9974dec96a2b32
skpixels <makkobir79@gmail.com>
2023-08-03 03:29:47 +0300

    modal added

--- src/components/workoutDetails.js
+++ src/components/workoutDetails.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatDistanceToNow } from "date-fns";
 
 const WorkoutDetails = ({ workout, refetch }) => {
   const { name, title, reps, load, createdAt, _id } = workout;
+  const [showModal, setShowModal] = useState(false);
 
-  const handleDelete = async () => {
+  const handleDeleteModal = async () => {
     const url = `http://localhost:5000/api/workouts/${_id}`;
     const response = await fetch(url, {
       method: "DELETE",
@@ -17,6 +18,13 @@ const WorkoutDetails = ({ workout, refetch }) => {
     if (response.ok) {
       refetch();
     }
+    setShowModal(false);
+  };
+
+  const handleCancel = () => {
+    // If the user cancels the deletion, simply close the modal
+    console.log("not");
+    setShowModal(false);
   };
 
   return (
@@ -31,7 +39,7 @@ const WorkoutDetails = ({ workout, refetch }) => {
             </p>
             <p>
               <strong>Reps: </strong>
-              {reps} k.g
+              {reps} set
             </p>
             <p>
               <strong>Load: </strong>
@@ -43,9 +51,58 @@ const WorkoutDetails = ({ workout, refetch }) => {
             </p>
           </div>
         </div>
-        <button class="btn btn-danger me-2" onClick={handleDelete}>
-          delete
-        </button>
+        <div>
+          <button
+            className="btn btn-danger me-2"
+            onClick={() => setShowModal(true)}
+          >
+            delete
+          </button>
+
+          {showModal && (
+            <div
+              className="modal"
+              tabIndex="-1"
+              role="dialog"
+              style={{ display: "block" }}
+            >
+              <div
+                className="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+                role="document"
+              >
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Confirm Delete</h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      onClick={handleCancel}
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    Are you sure you want to delete this item?
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={handleCancel}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={handleDeleteModal}
+                    >
+                      Confirm
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
--- src/components/workoutForm.js
+++ src/components/workoutForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const WorkoutForm = ({ refetch }) => {
   const [name, setName] = useState("");
@@ -6,6 +6,8 @@ const WorkoutForm = ({ refetch }) => {
   const [reps, setReps] = useState("");
   const [load, setLoad] = useState("");
   const [error, setError] = useState(null);
+
+  const toastRef = useRef(null);
 
   const handleWorkout = async (e) => {
     e.preventDefault();
@@ -29,12 +31,24 @@ const WorkoutForm = ({ refetch }) => {
     }
     if (response.ok) {
       refetch();
+      showToast();
+      e.target.reset();
     }
-    e.target.reset();
   };
+
+  const showToast = () => {
+    const toast = toastRef.current;
+    if (toast) {
+      // eslint-disable-next-line no-undef
+      const bootstrapToast = new bootstrap.Toast(toast);
+      bootstrapToast.show();
+    }
+  };
+
   return (
     <section>
       <h5 className="text-center text-primary ">ADD NEW WORKOUT</h5>
+
       <form onSubmit={handleWorkout}>
         <div>
           <label htmlFor="Name" className="mb-1">
@@ -94,6 +108,29 @@ const WorkoutForm = ({ refetch }) => {
           type="submit"
           value="ADD WORKOUT"
         />
+        <div className="toast-container position-fixed bottom-0 end-0 p-3">
+          <div
+            className="toast "
+            ref={toastRef}
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
+          >
+            <div className="toast-header">
+              <strong className="me-auto">{name} has added</strong>
+              <small>Just Now</small>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="toast"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="toast-body">
+              Your Workout has been added successfully
+            </div>
+          </div>
+        </div>
       </form>
       {error && <p>{error}</p>}
     </section>
